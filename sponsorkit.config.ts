51eddfd743b5f2aeb837fcce68f3ab787c1521b7
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2023-01-03 18:49:53 +0100

    chore: init

---
+++ sponsorkit.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig, presets } from "sponsorkit";
+
+export default defineConfig({
+  tiers: [
+    {
+      title: "Past Sponsors",
+      monthlyDollars: -1,
+      preset: presets.xs,
+    },
+    {
+      title: "Backers",
+      preset: presets.small,
+    },
+    {
+      title: "Sponsors",
+      monthlyDollars: 16,
+      preset: {
+        avatar: {
+          size: 42,
+        },
+        boxWidth: 52,
+        boxHeight: 52,
+        container: {
+          sidePadding: 30,
+        },
+      },
+    },
+    {
+      title: "Gold Sponsors",
+      monthlyDollars: 64,
+      preset: presets.medium,
+    },
+    {
+      title: "Favorite Sponsors",
+      monthlyDollars: 128,
+      preset: presets.large,
+    },
+    {
+      title: "Platinum Sponsors",
+      monthlyDollars: 256,
+      preset: presets.xl,
+    },
+  ],
+});
