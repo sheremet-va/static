1456aeaf229c8d75a30dd04a8d7a407f153734b7
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-09-07 18:03:08 +0200

    feat: update

--- sponsorkit.config.ts
+++ sponsorkit.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig, presets } from "sponsorkit";
+import { defineConfig, tierPresets } from "sponsorkit";
+import fs from 'fs/promises'
 
 export default defineConfig({
   tiers: [
     {
       title: "Past Sponsors",
       monthlyDollars: -1,
-      preset: presets.xs,
+      preset: tierPresets.xs,
     },
     {
       title: "Backers",
-      preset: presets.small,
+      preset: tierPresets.small,
     },
     {
       title: "Sponsors",
@@ -28,17 +29,64 @@ export default defineConfig({
     {
       title: "Gold Sponsors",
       monthlyDollars: 64,
-      preset: presets.medium,
+      preset: tierPresets.medium,
     },
     {
       title: "Platinum Sponsors",
       monthlyDollars: 256,
-      preset: presets.large,
+      preset: tierPresets.large,
     },
     {
       title: "Big Sponsors",
       monthlyDollars: 512,
-      preset: presets.xl,
+      preset: tierPresets.xl,
     },
   ],
+
+  async onSponsorsReady(sponsors) {
+    await fs.writeFile(
+      'sponsors.json',
+      JSON.stringify(
+        sponsors
+          .filter((i) => i.privacyLevel !== 'PRIVATE')
+          .map((i) => {
+            return {
+              name: i.sponsor.name,
+              login: i.sponsor.login,
+              avatar: i.sponsor.avatarUrl,
+              amount: i.monthlyDollars,
+              link: i.sponsor.linkUrl || i.sponsor.websiteUrl,
+              org: i.sponsor.type === 'Organization'
+            }
+          })
+          .sort((a, b) => b.amount - a.amount),
+        null,
+        2
+      )
+    )
+  },
+
+  outputDir: '.',
+  formats: ['svg', 'png'],
+
+  renders: [
+    {
+      name: 'sponsors',
+      width: 800,
+    },
+    {
+      name: 'sponsors.wide',
+      width: 1800,
+    },
+    {
+      name: 'sponsors.part1',
+      width: 800,
+      filter: (sponsor) => sponsor.monthlyDollars >= 9.9,
+    },
+    {
+      name: 'sponsors.part2',
+      width: 800,
+      filter: (sponsor) => sponsor.monthlyDollars < 9.9 && sponsor.monthlyDollars >= 0
+    },
+  ]
 });
