a9f95f0a9d73924796e2f2315c23a0c34bf7ad68
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2026-01-08 19:01:12 +0100

    chore: update chromatic link

--- sponsorkit.vitest.config.ts
+++ sponsorkit.vitest.config.ts
@@ -9,6 +9,13 @@ export default defineConfig({
     {
       monthlyDollars: 500,
       preset: tierPresets.large,
+      composeBefore(_, sponsors) {
+        sponsors.forEach(({ sponsor }) => {
+          if(sponsor.name === 'Chromatic') {
+            sponsor.websiteUrl = 'https://www.chromatic.com/?utm_source=vitest&utm_medium=sponsorship&utm_campaign=vitestSponsorship'
+          }
+        })
+      }
     },
     {
       preset: tierPresets.none,
