ee6dd150f90d9f992de621847d3cee3c081ab96f
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2026-05-26 15:13:20 +0200

    chore: try onSponsorsFetched

--- sponsorkit.vitest.config.ts
+++ sponsorkit.vitest.config.ts
@@ -1,6 +1,42 @@
-import { defineConfig, tierPresets, resolveAvatars, type Sponsorship } from "sponsorkit";
+import { defineConfig, tierPresets, type Sponsorship } from "sponsorkit";
 
 export default defineConfig({
+  onSponsorsFetched(sponsors) {
+    const customSponsors: Sponsorship[] = [
+      // vercel (via antfu)
+      {
+        monthlyDollars: 1000,
+        sponsor: {
+          type: 'Organization',
+          login: 'vercel',
+          name: 'Vercel',
+          avatarUrl: 'https://avatars.githubusercontent.com/u/14985020?v=4',
+          websiteUrl: 'https://vercel.com/',
+          linkUrl: 'https://github.com/vercel/'
+        },
+        isOneTime: false,
+        provider: 'github',
+        privacyLevel: 'PUBLIC',
+      },
+      // zammad (via sheremet-va)
+      {
+        monthlyDollars: 1000,
+        sponsor: {
+          type: 'Organization',
+          login: 'zammad',
+          name: 'Zammad',
+          avatarUrl: 'https://avatars.githubusercontent.com/u/1380327?v=4',
+          websiteUrl: 'https://zammad.com/en',
+          linkUrl: 'https://github.com/zammad/'
+        },
+        isOneTime: false,
+        provider: 'github',
+        privacyLevel: 'PUBLIC',
+      },
+    ]
+
+    return [...sponsors, ...customSponsors]
+  },
   tiers: [
     {
       monthlyDollars: -1,
@@ -22,41 +58,6 @@ export default defineConfig({
             sponsor.websiteUrl = 'https://www.chromatic.com/?utm_source=vitest&amp;utm_medium=sponsorship&amp;utm_campaign=vitestSponsorship'
           }
         })
-        const customSponsors: Sponsorship[] = [
-          // vercel (via antfu)
-          {
-            monthlyDollars: 1000,
-            sponsor: {
-              type: 'Organization',
-              login: 'vercel',
-              name: 'Vercel',
-              avatarUrl: 'https://avatars.githubusercontent.com/u/14985020?v=4',
-              websiteUrl: 'https://vercel.com/',
-              linkUrl: 'https://github.com/vercel/'
-            },
-            isOneTime: false,
-            provider: 'github',
-            privacyLevel: 'PUBLIC',
-          },
-          // zammad (via sheremet-va)
-          {
-            monthlyDollars: 1000,
-            sponsor: {
-              type: 'Organization',
-              login: 'zammad',
-              name: 'Zammad',
-              avatarUrl: 'https://avatars.githubusercontent.com/u/1380327?v=4',
-              websiteUrl: 'https://zammad.com/en',
-              linkUrl: 'https://github.com/zammad/'
-            },
-            isOneTime: false,
-            provider: 'github',
-            privacyLevel: 'PUBLIC',
-          },
-        ]
-        sponsors.push(...customSponsors)
-        await resolveAvatars(customSponsors, '')
-        console.log(sponsors)
       }
     },
     {
