8dd5d8795ad8cb7fd2ab3b3961202090d43b97c2
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2026-05-26 13:22:59 +0200

    chore: test workflow

--- sponsorkit.vitest.config.ts
+++ sponsorkit.vitest.config.ts
@@ -3,6 +3,13 @@ import { defineConfig, tierPresets } from "sponsorkit";
 export default defineConfig({
   tiers: [
     {
+      monthlyDollars: -1,
+      preset: tierPresets.xs,
+      composeBefore(_, sponsors) {
+        sponsors.length = 0
+      }
+    },
+    {
       monthlyDollars: 100,
       preset: tierPresets.medium,
     },
@@ -14,6 +21,30 @@ export default defineConfig({
           if(sponsor.name === 'Chromatic') {
             sponsor.websiteUrl = 'https://www.chromatic.com/?utm_source=vitest&amp;utm_medium=sponsorship&amp;utm_campaign=vitestSponsorship'
           }
+        })
+        sponsors.push({
+          monthlyDollars: 1000,
+          sponsor: {
+            type: 'Organization',
+            login: 'vercel',
+            name: 'Zammad',
+            avatarUrl: 'https://vitest.dev/zammad.svg',
+            websiteUrl: 'https://zammad.com/en'
+          },
+          provider: 'github',
+          privacyLevel: 'PUBLIC',
+        })
+        sponsors.push({
+          monthlyDollars: 1000,
+          sponsor: {
+            type: 'Organization',
+            login: 'zammad',
+            name: 'Zammad',
+            avatarUrl: 'https://vitest.dev/zammad.svg',
+            websiteUrl: 'https://zammad.com/en'
+          },
+          provider: 'github',
+          privacyLevel: 'PUBLIC',
         })
       }
     },
